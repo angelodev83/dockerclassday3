010ac071b1a71222c6640ec249593c1e8d31224a
angelodev83 <angeloti83@gmail.com>
2025-09-16 12:30:22 +0300

    half class done

--- Dockerfile
+++
@@ -1,7 +0,0 @@
-FROM #alpine image
-WORKDIR #directory in container
-COPY # path to package files
-RUN # npm install
-COPY . .
-EXPOSE # port 3000
-CMD #node and server for js
--- server.js
+++
@@ -1,6 +0,0 @@
-const http = require('http');
-const port = 3000;
-const server = http.createServer((req, res) => {
-  res.end('Hello from Node inside Docker!');
-});
-server.listen(port, () => console.log(`Listening on port ${port}`));
